faccc085ec0e4957a53f7ae5a9512f2a8ec9fd97
Madi <madi_gaziz@mail.ru>
2026-02-20 11:54:52 +0500

    Установил env конфигуратор

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,          // важно, чтобы Vite слушал извне
-    allowedHosts: true,
-    proxy: {
-      "/api": "http://127.0.0.1:8000",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const ngrok_path = env.VITE_NGROK_PATH;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true,
+      allowedHosts: true,
+      proxy: {
+        "/api": "http://127.0.0.1:8000",
+      },
+      hmr: {
+        protocol: "wss",
+        host: ngrok_path,
+        clientPort: 443,
+      },
     },
-    hmr: {
-      protocol: "wss",
-      host: "45f37caba858.ngrok-free.app", // <-- твой ngrok фронта
-      clientPort: 443,
-    },
-  },
+  };
 });
