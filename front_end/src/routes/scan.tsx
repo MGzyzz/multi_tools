051353ad1d28b6f25b3bea4df182c7a5b9cdd208
Madi <madi_gaziz@mail.ru>
2026-06-13 22:15:13 +0500

    add new parametrs

--- front_end/src/routes/scan.tsx
+++ front_end/src/routes/scan.tsx
@@ -756,11 +756,13 @@ function ScanPage() {
                     {scheduleOptions.length === 0 ? (
                       <option value="">{t("scan.noLessonsWeek")}</option>
                     ) : (
-                      scheduleOptions.map((entry) => (
-                        <option key={entry.id} value={entry.id}>
-                          {formatScheduleLabel(entry, locale)}
-                        </option>
-                      ))
+                      scheduleOptions
+                        .filter((entry) => entry.date >= weekRange.todayDate)
+                        .map((entry) => (
+                          <option key={entry.id} value={entry.id}>
+                            {formatScheduleLabel(entry, locale)}
+                          </option>
+                        ))
                     )}
                   </select>
                 </div>
